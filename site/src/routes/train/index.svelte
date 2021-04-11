62c12ae697c9337078a1640a6cfa8c63ce9f1ef9
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-04-11 13:27:54 -0700

    Add initial listing

---
+++ site/src/routes/train/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+	export async function load({ page, fetch }) {
+		const url = '/train/species.json';
+		const res = await fetch(url);
+		if (res.ok) {
+			return {
+				props: {
+					species: await res.json()
+				}
+			};
+		}
+		return {
+			status: res.status,
+			error: new Error(`Could not load ${url}`)
+		};
+	}
+</script>
+
+<script>
+	export let species;
+</script>
+
+{species}

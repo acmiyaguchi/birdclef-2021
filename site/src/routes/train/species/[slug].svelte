837eba30d36dd792a80ec2468f4bc7754edafdd9
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-04-13 21:34:56 -0700

    Add information to page

--- site/src/routes/train/species/[slug].svelte
+++ site/src/routes/train/species/[slug].svelte
@@ -1,25 +1,36 @@
 <script context="module">
   export async function load({ page, fetch }) {
-    const url = `/train/species/${page.params.slug}.json`;
-    const res = await fetch(url);
+    const { slug } = page.params;
+    const res = await fetch(`/train/species/${slug}.json`);
     if (res.ok) {
       return {
         props: {
-          slug: page.params.slug,
+          slug: slug,
           examples: await res.json()
         }
       };
     }
     return {
       status: res.status,
-      error: new Error(`Could not load ${url}`)
+      error: new Error(`Could not load data for ${slug}`)
     };
   }
 </script>
 
 <script>
+  import { onMount } from "svelte";
   export let slug;
   export let examples;
+  export let info = {};
+  export let metadata = [];
+
+  onMount(async () => {
+    let resp;
+    resp = await fetch(`/data/metadata/${slug}/info.json`);
+    info = await resp.json();
+    resp = await fetch(`/data/metadata/${slug}/metadata.json`);
+    metadata = await resp.json();
+  });
 </script>
 
 <svelte:head>
@@ -27,6 +38,12 @@
 </svelte:head>
 
 <h1>{slug}</h1>
+<p>
+  Common Name: <a href="https://www.wikipedia.org/wiki/{info.common_name}" target="_blank"
+    >{info.common_name}</a
+  >
+</p>
+<p>Scientific Name: {info.scientific_name}</p>
 
 <a href="/train">back to species</a>
 

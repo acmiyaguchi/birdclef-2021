602bcbb0a73e60934875c932087a4cf1b44c5403
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2021-04-15 00:08:25 -0700

    Add ability to save labels from page

--- site/src/routes/train/species/Labels.svelte
+++ site/src/routes/train/species/Labels.svelte
@@ -1,6 +1,8 @@
 <script>
   import { onMount } from "svelte";
   import localforage from "localforage";
+  import filesaver from "file-saver";
+  import { sortBy } from "lodash";
 
   export let slug;
   export let names;
@@ -15,8 +17,22 @@
       (await localforage.getItem(key)) ||
       Object.fromEntries(
         names
-          .map((name) => [`${name}.motif.0`, { is_valid: false }])
-          .concat(names.map((name) => [`${name}.motif.1`, { is_valid: false }]))
+          .map((name) => [`${name}.motif.0`, { is_valid: true }])
+          .concat(names.map((name) => [`${name}.motif.1`, { is_valid: true }]))
       );
   });
+
+  function save(labels) {
+    let data = sortBy(
+      Object.entries(labels).map(([key, value]) => ({
+        name: key,
+        ...value
+      })),
+      ["name"]
+    );
+    let blob = new Blob([JSON.stringify(data)], { type: "application/json" });
+    filesaver.saveAs(blob, `${key}.labels.json`);
+  }
 </script>
+
+<button on:click={() => save(labels)}>Save labels</button>
